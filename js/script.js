93f87a350791484b9513661db0f41baf2e4b32da
suryabhan29 <suryabhansingh1029@gmail.com>
2023-06-08 15:21:12 +0530

    java script naming convention

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let a; //declertation 
+
+//let b = 10 ; 
+
+//let b = 20; // Redecleration and initialization
+
+// in let keyword we do not redecleration 
+
+var c = 2;
+var c = 3;
+
+// in var keyword we do redecleration 
+
+//const d = 8;
+//const d = 8;
+
+// in const keyword we do not redecleation 
